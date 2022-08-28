7e475cf6cb345cae039952150769a852cb0f1980
Valentino Masetti <valentinomasetti@hotmail.it>
2022-08-28 12:02:32 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Valentino Masetti on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
